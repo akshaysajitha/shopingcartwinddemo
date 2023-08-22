3bea5e95d227df9e3e63e3e724c8a6e4e24de7f2
pavithracv123 <pavithracv123@gmail.com>
2023-08-22 13:21:54 +0530

    all link set in the header

--- frontendshopingcart/src/app/app-routing.module.ts
+++ frontendshopingcart/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './pages/welcome/container/cart/cart.component';
 import { HeaderComponent } from './pages/welcome/scene/header/header.component';
+import { OrderviewComponent } from './pages/welcome/orderitem/orderview/orderview.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   {path:'cart',component:CartComponent},
-  {path:'header',component:HeaderComponent}
+  {path:'header',component:HeaderComponent},
+  {path:'orderitemview',component:OrderviewComponent}
+
 ];
 
 @NgModule({
